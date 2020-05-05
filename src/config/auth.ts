ab1e5fecb7984eae4435a79c6af2b3ef956108f4
Emílio Gularte Kipper <emilio_gularte@outlook.com>
2020-05-05 18:23:05 -0300

    First commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '2ce0c3e6498754a7ab667e1830a93d6a',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import ensureAuthenticaded from '../middlewares/ensureAuthenticated';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import uploadConfig from '../config/upload';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const createUser = new CreateUserService();
+
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticaded,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatarService = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  },
+);
+
+export default usersRouter;
